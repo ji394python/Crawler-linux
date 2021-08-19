3a6977da1a9fdf31611a24b3d77cd0ef5ab3fdad
劉家銘 <ji394python@gmail.com>
2021-08-20 06:49:32 +0800

    0820 update bash

--- news_project/main.sh
+++ news_project/main.sh
@@ -2,6 +2,10 @@
 #!/bin/bash
 #手動操作時可用此
 cd news_crawler
+python news_crawler.py -st 2021/01/01 -et 2021/08/18
+cd ..
+python parsing.py
+cd news_crawler
 python TWSE_crawler.py 
 rm -f twse_future.csv
 cd ..
